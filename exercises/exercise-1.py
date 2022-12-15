11c31a3ce58615fe8e6e81cfe4160725efa623b1
Anthony Siletti <asiletti@icloud.com>
2022-12-15 12:29:42 -0500

    initial commit

---
+++ exercises/exercise-1.py
@@ -0,0 +1,23 @@
+# exercise-01 Vowel or Consonant
+
+# Write the code that:
+# 1. Prompts the user to enter a letter in the alphabet:
+#      Please enter a letter from the alphabet (a-z or A-Z):
+# 2. Write the code that determines whether the letter entered is a vowel
+# 3. Print one of following messages (substituting the letter for x):
+#      - The letter x is a vowel
+#      - The letter x is a consonant
+
+# Hints:  Use the in operator to check if a character is in another string
+#         For example, if some_char in 'abc':
+
+vowels = ["a", "e", "i", "o", "u"]
+
+while True:
+    user_input = input('Enter a single character. Type ".quit" to quit. > ').lower()
+    if user_input == ".quit":
+        break
+    if user_input in vowels:
+        print(f"The letter {user_input} is a vowel")
+    elif user_input:
+        print(f"The letter {user_input} is a consonant")
